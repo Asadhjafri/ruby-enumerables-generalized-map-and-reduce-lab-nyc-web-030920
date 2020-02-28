38b2cf11396e454b9e83d0b6b001414bcecb7db9
Asad Jafri <asadhjafri@gmail.com>
2020-02-28 20:54:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
-# Your Code Here
+def map(source)
+  own_map = []
+  i = 0
+  while i < source.length
+    own_map.push(yield(source[i]))
+    i += 1
+  end
+  own_map
+end
+
+def reduce(source, starting_point = nil)
+  total = 0
+  if starting_point
+    total = starting_point
+    i = 0
+  end
+
+end
